be71becb2a6946c4c82c365aa36a27b9c374d306
companyabab <30520002+companyabab@users.noreply.github.com>
2017-08-28 23:22:38 -0400

    Update script6.sh

--- script6.sh
+++ script6.sh
@@ -2,10 +2,8 @@
 
 # Check the bash shell script is being run by root or not
 
-if[[$EUID -NE 0]];
+if[[$EUID -ne 0]];
 then
 echo "you must be root user" 2>&!
 exit 1
-else
-mount /dev/sdb1 /mnt/disk2
 fi 
